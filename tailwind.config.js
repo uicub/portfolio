ff0687f2c63f8eb82e1a7079d8f86be79864a2d3
uicub <flow.uicub@gmail.com>
2022-03-02 16:37:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontSize: {
+      "number-xl": "7.313rem",
+    },
+    extend: {
+      fontFamily: {
+        sen: ["Sen", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
